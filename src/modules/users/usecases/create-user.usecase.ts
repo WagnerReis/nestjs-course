01101c7a2e6060234c2eecf92b3647eb80fa9dfe
Wagner Reis <wagnerreis1997@gmail.com>
2024-09-24 15:40:20 -0300

    feat: add create user use case

--- src/modules/users/usecases/create-user.usecase.ts
+++ src/modules/users/usecases/create-user.usecase.ts
@@ -2,33 +2,27 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/infra/database/prisma.service';
 import { CreateUserDTO } from '../dto/user.dto';
 import { hash } from 'bcrypt';
+import { IUserRepository } from '../repository/user.repository';
 
 @Injectable()
 export class CreateUserUseCase {
-  constructor(private prisma: PrismaService) {}
+  constructor(private userRepository: IUserRepository) {}
 
   async execute(data: CreateUserDTO): Promise<Omit<CreateUserDTO, 'password'>> {
-    const user = await this.prisma.user.findFirst({
-      where: {
-        OR: [{ username: data.username }, { email: data.email }],
-      },
+    const user = await this.userRepository.findByUsernameOrEmail({
+      username: data.username,
+      email: data.email,
     });
 
     if (user) {
       throw new HttpException('User already exists!', HttpStatus.BAD_REQUEST);
     }
 
-    const passwordHashed = await hash(data.password, 10);
+    const password = await hash(data.password, 10);
 
-    const newUser = await this.prisma.user.create({
-      data: {
-        ...data,
-        password: passwordHashed,
-      },
+    return await this.userRepository.save({
+      ...data,
+      password,
     });
-
-    Reflect.deleteProperty(newUser, 'password');
-
-    return newUser;
   }
 }
--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { CreateUserUseCase } from './usecases/create-user.usecase';
 import { PrismaService } from 'src/infra/database/prisma.service';
+import { UserPrismaRepository } from './repository/prisma/user.prisma.repository';
+import { IUserRepository } from './repository/user.repository';
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [CreateUserUseCase, PrismaService],
+  providers: [
+    CreateUserUseCase,
+    PrismaService,
+    UserPrismaRepository,
+    {
+      provide: IUserRepository,
+      useClass: UserPrismaRepository,
+    },
+  ],
 })
 export class UserModule {}
