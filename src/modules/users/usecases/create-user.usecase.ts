ad616898071b0e160cf20cc2297c8f68972de81d
Wagner Reis <wagnerreis1997@gmail.com>
2024-09-23 21:35:23 -0300

    feat: add exception and hash to the password

--- src/modules/users/usecases/create-user.usecase.ts
+++ src/modules/users/usecases/create-user.usecase.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/infra/database/prisma.service';
 import { CreateUserDTO } from '../dto/user.dto';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class CreateUserUseCase {
   constructor(private prisma: PrismaService) {}
 
-  async execute(data: CreateUserDTO) {
+  async execute(data: CreateUserDTO): Promise<Omit<CreateUserDTO, 'password'>> {
     const user = await this.prisma.user.findFirst({
       where: {
         OR: [{ username: data.username }, { email: data.email }],
@@ -14,9 +15,20 @@ export class CreateUserUseCase {
     });
 
     if (user) {
-      throw new Error('User already exists!');
+      throw new HttpException('User already exists!', HttpStatus.BAD_REQUEST);
     }
 
-    return await this.prisma.user.create({ data });
+    const passwordHashed = await hash(data.password, 10);
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        ...data,
+        password: passwordHashed,
+      },
+    });
+
+    Reflect.deleteProperty(newUser, 'password');
+
+    return newUser;
   }
 }
